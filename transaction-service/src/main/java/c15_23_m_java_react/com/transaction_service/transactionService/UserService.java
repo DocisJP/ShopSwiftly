95423418e681c7cab044a3f65a78c0c0c0315d8f
Pablo <godoypablojuan@gmail.com>
2023-12-18 17:31:27 -0300

    Modified a path for getAllProducts in ProductController

--- transaction-service/src/main/java/c15_23_m_java_react/com/transaction_service/transactionService/UserService.java
+++ transaction-service/src/main/java/c15_23_m_java_react/com/transaction_service/transactionService/UserService.java
@@ -1,5 +1,7 @@
 package c15_23_m_java_react.com.transaction_service.transactionService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.stereotype.Service;
 
@@ -9,13 +11,22 @@ import c15_23_m_java_react.com.transaction_service.dtos.UserDto;
 public class UserService {
     
     private final UserClient userClient;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(UserClient userClient) {
         this.userClient = userClient;
     }
 
     public UserDto getUserDto(Long id) {
-        return userClient.getUsertDto(id);
+        logger.info("Fetching user with ID: {}", id);
+        try {
+            return userClient.getUsertDto(id);
+        } catch (Exception ex) {
+            logger.error("Error fetching user with ID: {}", id, ex);
+            throw ex;
+        }
     }
+
+    
 }
 
