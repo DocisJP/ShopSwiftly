1af974f0db324e037ea7d0e0eaa13d6385c6eaa4
Pablo <godoypablojuan@gmail.com>
2023-12-17 22:40:43 -0300

    Modified TransactionDTO constructors to fix an error

--- transaction-service/src/main/java/c15_23_m_java_react/com/transaction_service/dtos/TransactionDto.java
+++ transaction-service/src/main/java/c15_23_m_java_react/com/transaction_service/dtos/TransactionDto.java
@@ -11,6 +11,13 @@ public class TransactionDto {
 
     private List<TransactionItemDto> itemDtos;
 
+    private Double impuestos;
+
+    private Double montoTotal;
+
+    private Double montoDescontado;
+
+    // Getters and setters for productoDto
     public ProductDto getProductoDto() {
         return this.productoDto;
     }
@@ -19,15 +26,7 @@ public class TransactionDto {
         this.productoDto = productoDto;
     }
 
-	public UserDto getuserDto() {
-        return this.userDto;
-    }
-
-    public void setProductoDto(UserDto userDto) {
-        this.userDto = userDto;
-    }
-
-
+    // Getters and setters for userDto
     public UserDto getUserDto() {
         return this.userDto;
     }
@@ -36,12 +35,38 @@ public class TransactionDto {
         this.userDto = userDto;
     }
 
+    // Getters and setters for itemDtos
     public List<TransactionItemDto> getItemDtos() {
         return this.itemDtos;
     }
 
     public void setItemDtos(List<TransactionItemDto> itemDtos) {
         this.itemDtos = itemDtos;
+    }
+
+
+    public Double getImpuestos() {
+        return this.impuestos;
+    }
+
+    public void setImpuestos(Double impuestos) {
+        this.impuestos = impuestos;
+    }
+
+    public Double getMontoTotal() {
+        return this.montoTotal;
+    }
+
+    public void setMontoTotal(Double montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    public Double getMontoDescontado() {
+        return this.montoDescontado;
+    }
+
+    public void setMontoDescontado(Double montoDescontado) {
+        this.montoDescontado = montoDescontado;
     }
 
 }
